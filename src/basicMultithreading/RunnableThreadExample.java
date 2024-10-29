777bfbaba99fd0c0d4aa120d95f30485844a1190
loucortes <lucianohcortes@gmail.com>
2024-10-29 13:41:19 +0100

    First examples of basic Multithreading

---
+++ src/basicMultithreading/RunnableThreadExample.java
@@ -0,0 +1,36 @@
+package basicMultithreading;
+
+public class RunnableThreadExample {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new ThreadOne());
+        Thread t2 = new Thread(new ThreadTwo());
+        Thread t3 = new Thread(() -> {
+            for (int i = 0; i < 15; i++) {
+                System.out.println("ThreadThree: " + i);
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
+
+class ThreadOne implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("ThreadOne: " + i);
+        }
+    }
+}
+
+class ThreadTwo implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 15; i++) {
+            System.out.println("ThreadTwo: " + i);
+        }
+    }
+}
